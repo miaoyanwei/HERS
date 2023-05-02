322c463afb8fab0a38f6245f81e103e131a72762
“Yanwei <miaoywstyle@msn.com>
2023-05-02 03:33:28 +0200

    convert json to desired json structure

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
+import json
 
-# Create your views here.
 
 # Miao: Communicate with frontend (HTTP)
 from django.http import HttpResponse
 import datetime
 
-# Miao: Post
+
+# Miao: Post method
 def flex(request):
     if (request.method != 'POST'):
         return HttpResponse("", status=405) #only POST methods are sent
     print("body: " + request.body.decode('utf-8'))
     return HttpResponse()
+
+
+# Miao: JSON data structure from frontend
+json_data = '{"cat_region_country":"Deutschland", "cat_region_state":"Hessen"}' #test
+
+# Miao: Load the JSON data into a Python dictionary
+data = json.loads(json_data)
+new_json = json.loads('{}')
+
+# Miao: Iterate through the key-value pairs
+for key, value in data.items():
+    category = key.split("_")[1]
+    field = key.split("_")[2]
+    if new_json.get(category) is None:
+        new_json[category] = json.loads('{}')
+    new_json[category][field] = value
+        
+print(new_json)
