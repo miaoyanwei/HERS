7701b5f013b05bb0e1e536618b50615a29ff1a70
“Yanwei <miaoywstyle@msn.com>
2023-05-11 16:29:40 +0200

    change from Google chart to Highcharts

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,38 +1,81 @@
-// include Google Charts library
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+// File: chart.js
 
+document.addEventListener('DOMContentLoaded', function () {
 
-function drawChart() {
-var data = google.visualization.arrayToDataTable([
-    ['Time', 'Heating', 'Cooling', 'Appliance', 'Hot water'],
-    ['JAN', 1000, 400, 800, 200],
-    ['FEB', 1170, 460, 800, 200],
-    ['MAR', 660, 1120, 800, 200],
-    ['APR', 1030, 540, 800, 200],
-    ['MAY', 1000, 400, 800, 200],
-    ['JUN', 1170, 460, 800, 200],
-    ['JUL', 660, 1120, 800, 200],
-    ['AUG', 1030, 540, 800, 200],
-    ['SEP', 1000, 400, 800, 200],
-    ['OCT', 1170, 460, 800, 200],
-    ['NOV', 660, 1120, 800, 200],
-    ['DEC', 1030, 540, 800, 200],
-]);
+  Highcharts.setOptions({
+    colors: ['#A78067', '#E2E2E2', '#ADA3A3', '#DDB9A1', '#6DAE47']
+  });
 
-var options = {
-    title: ' ',
-    hAxis: {title: ' ',  titleTextStyle: {color: '#C2C2C2', fontName: 'Philosopher'}},
-    vAxis: {minValue: 0, textStyle: {fontName: 'Philosopher'} },
-    legend: 'none', // hide the legend
-    isStacked: true, // added property for stacked chart
-    colors: ['#A78067', '#E2E2E2', '#ADA3A3', '#DDB9A1'], // set colors for columns
-    titleTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set title font size and color
-    legendTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set legend font size and color
-    vAxis: { textStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' } }, // set vertical axis font size and color
-    hAxis: { textStyle: { fontSize: 8, color: '#C2C2C2', fontName: 'Philosopher' } } // set horizontal axis font size and color
-  };
+  Highcharts.chart('chart_current_year', {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: ' '
+    },
+    // Hide the legend
+    legend: {
+      enabled: false
+    },
+    xAxis: {
+      categories: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+      labels: {
+        style: {
+          fontFamily: 'Philosopher',
+          fontSize: '10px'
+        }
+      }
+    },
+    // Primary Y-axis
+    yAxis: [{ 
+      title: { 
+        text: null
+      },
+      labels: { // Disable labels on the right side of the graph
+        enabled: false
+      },
+    }, { // Secondary Y-axis
+      title: { 
+        text: null
+      },
+      opposite: false,
+      labels: { // Disable labels on the right side of the graph
+        enabled: false
+      }
+    }],
+    series: [{
+      name: 'Heating',
+      data: [1000, 1170, 660, 1030, 1000, 1170, 660, 1030, 1000, 1170, 660, 1030],
+      stack: 'amount',
+    }, {
+      name: 'Cooling',
+      data: [400, 460, 1120, 540, 1000, 1170, 660, 1030, 1000, 1170, 660, 1030],
+      stack: 'amount'
+    }, {
+      name: 'Appliance',
+      data: [400, 460, 1120, 540, 1000, 1170, 660, 1030, 1000, 1170, 660, 1030],
+      stack: 'amount'
+    }, {
+      name: 'Hot water',
+      data: [400, 460, 1120, 540, 1000, 1170, 660, 1030, 1000, 1170, 660, 1030],
+      stack: 'amount'
+    }, {
 
-var chart = new google.visualization.ColumnChart(document.getElementById('chart_current_year'));
-chart.draw(data, options);
-}
+      // Second bar
+      name: 'PV',
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      yAxis: 1
+    }],
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+      }
+    },
+    tooltip: {
+      style: {
+        fontFamily: 'Philosopher', // Set the desired font family
+        fontSize: '12px' // Set the desired font size
+      }
+    },
+  });
+});
---
+++ static/js/googlechart.js
@@ -0,0 +1,75 @@
+// include Google Charts library
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawCurChart);
+google.charts.setOnLoadCallback(drawSimuChart);
+
+
+// current
+function drawCurChart() {
+var data = google.visualization.arrayToDataTable([
+    ['Time', 'Heating', 'Cooling', 'Appliance', 'Hot water'],
+    ['JAN', 1000, 400, 800, 200],
+    ['FEB', 1170, 460, 800, 200],
+    ['MAR', 660, 1120, 800, 200],
+    ['APR', 1030, 540, 800, 200],
+    ['MAY', 1000, 400, 800, 200],
+    ['JUN', 1170, 460, 800, 200],
+    ['JUL', 660, 1120, 800, 200],
+    ['AUG', 1030, 540, 800, 200],
+    ['SEP', 1000, 400, 800, 200],
+    ['OCT', 1170, 460, 800, 200],
+    ['NOV', 660, 1120, 800, 200],
+    ['DEC', 1030, 540, 800, 200],
+]);
+
+var options = {
+    title: ' ',
+    hAxis: {title: ' ',  titleTextStyle: {color: '#C2C2C2', fontName: 'Philosopher'}},
+    vAxis: {minValue: 0, textStyle: {fontName: 'Philosopher'} },
+    legend: 'none', // hide the legend
+    isStacked: true, // added property for stacked chart
+    colors: ['#A78067', '#E2E2E2', '#ADA3A3', '#DDB9A1'], // set colors for columns
+    titleTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set title font size and color
+    legendTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set legend font size and color
+    vAxis: { textStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' } }, // set vertical axis font size and color
+    hAxis: { textStyle: { fontSize: 8, color: '#C2C2C2', fontName: 'Philosopher' } } // set horizontal axis font size and color
+  };
+
+var chart = new google.visualization.ColumnChart(document.getElementById('chart_current_year'));
+chart.draw(data, options);
+}
+
+// simu
+function drawSimuChart() {
+  var data = google.visualization.arrayToDataTable([
+      ['Time', 'Heating', 'Cooling', 'Appliance', 'Hot water'],
+      ['JAN', 1000, 400, 800, 200],
+      ['FEB', 1170, 460, 800, 200],
+      ['MAR', 660, 1120, 800, 200],
+      ['APR', 1030, 540, 800, 200],
+      ['MAY', 1000, 400, 800, 200],
+      ['JUN', 1170, 460, 800, 200],
+      ['JUL', 660, 1120, 800, 200],
+      ['AUG', 1030, 540, 800, 200],
+      ['SEP', 1000, 400, 800, 200],
+      ['OCT', 1170, 460, 800, 200],
+      ['NOV', 660, 1120, 800, 200],
+      ['DEC', 1030, 540, 800, 200],
+  ]);
+  
+  var options = {
+      title: ' ',
+      hAxis: {title: ' ',  titleTextStyle: {color: '#C2C2C2', fontName: 'Philosopher'}},
+      vAxis: {minValue: 0, textStyle: {fontName: 'Philosopher'} },
+      legend: 'none', // hide the legend
+      isStacked: true, // added property for stacked chart
+      colors: ['#A78067', '#E2E2E2', '#ADA3A3', '#DDB9A1'], // set colors for columns
+      titleTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set title font size and color
+      legendTextStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' }, // set legend font size and color
+      vAxis: { textStyle: { fontSize: 10, color: '#C2C2C2', fontName: 'Philosopher' } }, // set vertical axis font size and color
+      hAxis: { textStyle: { fontSize: 8, color: '#C2C2C2', fontName: 'Philosopher' } } // set horizontal axis font size and color
+    };
+  
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_current_year'));
+  chart.draw(data, options);
+  }
