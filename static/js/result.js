f5c33a1c20b00214be313c88d429d9f33ba963dd
“Yanwei <miaoywstyle@msn.com>
2023-06-05 18:38:03 +0200

    add recommended configuration

--- static/js/result.js
+++ static/js/result.js
@@ -59,24 +59,38 @@ $.get("/app/myapp/flex", function(data){
 
     // Get recommendation list
 
-    const rec_list = data.recommendation;
+    const rec_list = data.recommendationList;
     let reclisthtml = '';
     rec_list.forEach((item,index)=>{
         entry = '<div class="improved">'
-        + '<h2 class="bill">Save <span>&#8364;</span><span id="savedcost-placeholder"></span>'
-        + item.recommendationBill.energy_bill_year
-        + '<span style="font-size: 16px; font-weight: normal;"> / year</span></h2>'
-        + '<p>If the following configurations are applied, the annual energy bill is estimated to be <span>&#8364;</span><span id="totalcost-placeholder"></span></p>'
-        + '<button type="button" class="btn btn-outline-dark" onclick="window.location.href=\'simulation.html\';">More details '
-        +  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">'
-        +       '<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>'
-        +  '</svg>'
-        + '</button>'
-        + '<div class="config">'
-        + '</div>'
-        + '</div>'
+            + '<h2 class="bill">Save <span>&#8364;</span><span id="savedcost-placeholder"></span>'
+            + item.recommendationBill.energy_bill_year
+            + '<span style="font-size: 16px; font-weight: normal;"> / year</span></h2>'
+            + '<p>If the following configurations are applied, the annual energy bill is estimated to be <span>&#8364;</span><span id="totalcost-placeholder"></span>'
+            + item.recommendationBill.energy_bill_saved_year
+            + '</p>'
+            + '<button type="button" class="btn btn-outline-dark" onclick="window.location.href=\'simulation.html\';">More details '
+            + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">'
+            + '<path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>'
+            + '</svg>'
+            + '</button>'
+            
+            // Get configurations
+
+            let confightml = '';
+            item.configurationList.forEach((technology,index)=>{
+                config = '<div class="config">'
+                    + technology
+                    + '<br>'
+                    + '</div>'
+                confightml += config
+            });
+            + confightml
+            + '</div>'
+
         reclisthtml += entry
     });
-    $("#recommandation-list").html(reclisthtml)
-    });
 
+    $("#recommandation-list").html(reclisthtml)
+    
+});
