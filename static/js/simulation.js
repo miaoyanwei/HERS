8362396d253cd49d3b292ae1da9ffef809d532d3
“Yanwei <miaoywstyle@msn.com>
2023-06-17 00:11:11 +0200

    update JSON and many

--- static/js/simulation.js
+++ static/js/simulation.js
@@ -1,28 +1,51 @@
-// TAB
+//// TAB
+//
+//// Function to open tabs
+//function openTab(evt, tabName, defaultTab) {
+//  var i, tabcontent, tablinks;
+//
+//  // Hide all tab contents for the current tab set
+//  tabcontent = document.getElementById(tabName).parentNode.getElementsByClassName("tabcontent");
+//  for (i = 0; i < tabcontent.length; i++) {
+//    tabcontent[i].style.display = "none";
+//  }
+//
+//  // Remove "active" class from tab links for the current tab set
+//  tablinks = document.getElementById(tabName).parentNode.getElementsByClassName("tablinks");
+//  for (i = 0; i < tablinks.length; i++) {
+//    tablinks[i].className = tablinks[i].className.replace(" active", "");
+//  }
+//
+//  // Show the selected tab content
+//  document.getElementById(tabName).style.display = "block";
+//
+//  // Add "active" class to the clicked tab link
+//  evt.currentTarget.className += " active";
+//}
+//
+//// Get the default open tab for each set and trigger click event
+//document.getElementById("defaultOpen1").click();
+//document.getElementById("defaultOpen2").click();
 
-// Function to open tabs
-function openTab(evt, tabName, defaultTab) {
-  var i, tabcontent, tablinks;
 
-  // Hide all tab contents for the current tab set
-  tabcontent = document.getElementById(tabName).parentNode.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+// Calculate annualised costs of PV and battery system
 
-  // Remove "active" class from tab links for the current tab set
-  tablinks = document.getElementById(tabName).parentNode.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+function getAnnuCost() {
 
-  // Show the selected tab content
-  document.getElementById(tabName).style.display = "block";
+  // Annualised costs (/kWp and /kWh)
+  var perPV = 115.61;
+  var perBattery = 41.29;
 
-  // Add "active" class to the clicked tab link
-  evt.currentTarget.className += " active";
+  // Retrieve the input values
+  var pv_size = 5;
+  var battery_capacity = 5;
+
+  // Perform the calculation
+  var annucostPV = parseFloat(perPV) * parseFloat(pv_size);
+  var annucostBattery = parseFloat(perBattery) * parseFloat(battery_capacity);
+
+  // Update the HTML with the result
+  document.getElementById("annucostPV").innerHTML = annucostPV;
+  document.getElementById("annucostBattery").innerHTML = annucostBattery;
+  
 }
-
-// Get the default open tab for each set and trigger click event
-document.getElementById("defaultOpen1").click();
-document.getElementById("defaultOpen2").click();
